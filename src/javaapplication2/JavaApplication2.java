a18f64f8f038c538781112bd758fdfc37adefe0c
ronak1 <ronak.sheth@sheridancollege.ca>
2021-01-29 01:36:01 -0500

    remove the extra added line

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,7 +16,6 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("hello");
-          System.out.println("hello2");
     }
     
 }
