e86aef8e7b0df64d60bdf8cbe4a373faaa7b1e9f
ronak1 <ronak.sheth@sheridancollege.ca>
2021-01-29 01:50:19 -0500

    adedd one more line1

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,8 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello123");
+        
     }
     
 }
