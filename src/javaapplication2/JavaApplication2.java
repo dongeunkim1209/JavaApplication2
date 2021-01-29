ccb5d2376e9a5856282a83873cbd8cbe02aec035
ronakgithub81 <46760686+ronakgithub81@users.noreply.github.com>
2021-01-29 01:57:47 -0500

    added hello5

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -17,7 +17,7 @@ public class JavaApplication2 {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello123");
-         System.out.println("hello4");
+         System.out.println("hello5");
         
     }
     
