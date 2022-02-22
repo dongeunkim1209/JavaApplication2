5f9c71c5abb231a276ff4bac16917e89131f7dfb
dongeun <dekim1209@gmail.com>
2022-02-22 10:35:09 -0500

    add comments

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -4,21 +4,23 @@
  * and open the template in the editor.
  */
 package javaapplication2;
-
+//change
 /**
  *
  * @author Ronak
  */
-public class JavaApplication2 {
+public class JavaApplication2
+{
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello123");
-         System.out.println("hello5");
-        
-    }
-    
+   /**
+    * @param args the command line arguments
+    */
+   public static void main (String[] args)
+   {
+      System.out.println("hello");
+      System.out.println("hello123");
+      System.out.println("hello5");
+
+   }
+
 }
